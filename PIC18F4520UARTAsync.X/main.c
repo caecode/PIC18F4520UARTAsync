5dca335b2e4f8fdc8536ccfc832dcdc5b0475694
caecode <harold.serrano@microchip.com>
2018-09-06 13:55:39 -0700

    modified the config bits

--- PIC18F4520UARTAsync.X/main.c
+++ PIC18F4520UARTAsync.X/main.c
@@ -4,7 +4,7 @@
 // 'C' source line config statements
 
 // CONFIG1H
-#pragma config OSC = XT         // Oscillator Selection bits (XT oscillator)
+#pragma config OSC = HS         // Oscillator Selection bits (HS oscillator)
 #pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
 #pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
 
@@ -62,6 +62,7 @@
 // Use project enums instead of #define for ON and OFF.
 
 #include <xc.h>
+
 
 #define _XTAL_FREQ 8000000
 char receiveFlag=0;
